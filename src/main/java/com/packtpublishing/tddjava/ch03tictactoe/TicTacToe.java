e4f9d6fae826a774b99d6615552f47eb3a915ef2
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2020-09-24 15:41:49 +0200

    erster versuch

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -4,4 +4,15 @@ package com.packtpublishing.tddjava.ch03tictactoe;
  * Created by vfarcic on 19/03/15.
  */
 public class TicTacToe {
+
+	String ausserhalbError = " ist außerhalb des Spielfeldes";
+
+	public void place(int x, int y) throws RuntimeException {
+		if (x > 2 || x < 0)
+			throw new RuntimeException("X = " + Integer.toString(x) + ausserhalbError);
+		if (y > 2 || y < 0)
+			throw new RuntimeException("Y = " + Integer.toString(y) + ausserhalbError);
+
+		return;
+	}
 }
--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,4 +1,40 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TicTacToeSpec {
+
+	TicTacToe game;
+
+	@Before
+	public void setUp() {
+		game = new TicTacToe();
+	}
+
+	@Test
+	public void beimSteinSetzenWennXzuGrossAusserhalbWerfeException() {
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> game.place(2, 2));
+		org.junit.Assert.assertEquals("X = 3 ist außerhalb des Spielfeldes", thrown.getMessage());
+	}
+
+	@Test
+	public void beimSteinSetzenWennYzuGrossAusserhalbWerfeException() {
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> game.place(2, 3));
+		org.junit.Assert.assertEquals("Y = 3 ist außerhalb des Spielfeldes", thrown.getMessage());
+	}
+
+	@Test
+	public void beimSteinSetzenWennXzuKleinAusserhalbWerfeException() {
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> game.place(-1, 2));
+		org.junit.Assert.assertEquals("X = -1 ist außerhalb des Spielfeldes", thrown.getMessage());
+	}
+
+	@Test
+	public void beimSteinSetzenWennYzuKleinAusserhalbWerfeException() {
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> game.place(0, -1));
+		org.junit.Assert.assertEquals("Y = -1 ist außerhalb des Spielfeldes", thrown.getMessage());
+	}
 }
